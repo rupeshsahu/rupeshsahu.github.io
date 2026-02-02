0458b62a62b95c56efcdedf98125ba26fc34fefe
Rupesh Sahu <rupeshsahu7509@gmail.com>
2026-02-02 15:01:31 +0530

    new portfolio

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-
-// Simple JS for potential future interactivity, currently not used beyond CSS animations.
-// Could be used for dynamic content loading, more complex animations, etc.
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Portfolio loaded. Initiating sequence.');
-});
